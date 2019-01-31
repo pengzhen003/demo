12a9ea351fa529cf94957c4a160da619ca4ad956
pengzhen003 <zhulong001@aliyun.com>
2019-01-31 13:25:26 +0800

    update file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,10 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+// @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    // SpringApplication.run(DemoApplication.class, args);
+    System.out.println("111111111111111111");
+  }
 }
-
